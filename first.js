2e2fe00519a93db45eae6d35463eb29dd909b2c6
Shabab391 <136697622+Shabab391@users.noreply.github.com>
2025-08-30 11:29:13 +0600

    your commit message

--- first.js
+++ first.js
@@ -15,3 +15,44 @@ heartIncrease("heart-btn6");
 heartIncrease("heart-btn7");
 heartIncrease("heart-btn8");
 heartIncrease("heart-btn9");
+
+
+function coinDecrease(id){
+    document.getElementById(id).addEventListener("click",function(){
+    let coinCount = parseInt(document.getElementById("coin").innerText)
+    if(coinCount<20){
+        alert("❌You do not have the required coins! You need at least 20 coins to call")
+        
+
+    }
+    else{
+
+    coinCount-=20;
+    document.getElementById("coin").innerText= coinCount
+
+
+    const callBtn = document.getElementById("call1");
+    const grandparent = callBtn.parentNode.parentNode;
+    const text = grandparent.querySelector(".text-xl");
+    console.log(text);
+    const number = grandparent.querySelector(".text-2xl");
+    console.log(number);
+
+
+    alert("📞 Calling "+text.innerText+" "+number.innerText)
+
+    
+
+
+        }
+})
+}
+coinDecrease("call1");
+coinDecrease("call2");
+coinDecrease("call3");
+coinDecrease("call4");
+coinDecrease("call5");
+coinDecrease("call6");
+coinDecrease("call7");
+coinDecrease("call8");
+coinDecrease("call9");
